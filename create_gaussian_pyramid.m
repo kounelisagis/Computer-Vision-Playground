7d0903573513df1b390eab914d67342b64d91c82
kounelisagis <kounelisagis@gmail.com>
2022-02-09 19:04:49 +0200

    complete 1d pyramids

--- create_gaussian_pyramid.m
+++ create_gaussian_pyramid.m
@@ -1,15 +1,20 @@
-function x_hat = find_next_pyramid_layer(x)
+function g_pyramid = create_gaussian_pyramid(x, pyramid_height)
+    g_pyramid = cell(pyramid_height,1);
+    g_pyramid{1} = x;
+    
+    for i = 2:pyramid_height
+        g_pyramid{i} = find_next_gaussian_pyramid_layer(g_pyramid{i-1});
+    end
+end
+
+
+function x_hat = find_next_gaussian_pyramid_layer(x)
     
     N = length(x);
-    
     T = toeplitz((1/16)*e(N, 1), h(N)');
-
-
     y = T*x;
 
-
     D = NaN(N/2, N/2);
-    
     for r = 1:N/2
         D(r,1:N/2) = e(N/2, r)';
     end
@@ -20,6 +25,7 @@ end
 
 
 function res = e(N, i)
+    assert(1 <= i && i <= N);
     res = [zeros(i-1, 1); 1; zeros(N-i, 1)];
 end
 
--- testbench.m
+++ testbench.m
@@ -3,18 +3,13 @@ x = atan(-5*pi:10*pi/(N-1):5*pi)';
 
 
 pyramid_height = 5;
-g_pyramid = cell(pyramid_height,1);
-g_pyramid{1} = x;
 
-for i = 2:pyramid_height
-    x = find_next_pyramid_layer(x);
-    g_pyramid{i} = x;
-end
-
+g_pyramid = create_gaussian_pyramid(x, pyramid_height);
 l_pyramid = gaussian_to_laplacian(g_pyramid, pyramid_height);
 g_pyramid_hat = laplacian_to_gaussian(l_pyramid, pyramid_height);
 
 
+sum_immse = 0;
 for i = 1:pyramid_height
-    immse(g_pyramid_hat{i}, g_pyramid{i})
+    sum_immse = sum_immse + immse(g_pyramid_hat{i}, g_pyramid{i});
 end
